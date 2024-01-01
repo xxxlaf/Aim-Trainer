f7f748743209dd7e7f2c73430ddf12dfb8908562
watersdr-uc <watersdr@mail.uc.edu>
2024-01-01 17:24:00 -0500

    home screen set up

---
+++ index.js
@@ -0,0 +1,34 @@
+// get buttons
+const btn_play = document.getElementById("btn play");
+const btn_settings = document.getElementById("btn settings");
+const btn_color_mode = document.getElementById("btn color-mode");
+
+function SetDefaults() {
+    if (localStorage.getItem("app color-mode") === null) {
+        localStorage.setItem("app color-mode", "light");
+    }
+}
+
+function ChangeColorMode() {
+    if (localStorage.getItem("app color-mode") === "light") {
+        document.documentElement.setAttribute("data-bs-theme", "dark");
+        localStorage.setItem("app color-mode", "dark");
+        btn_color_mode.className = "btn btn-light";
+        btn_color_mode.innerText = "Light Mode";
+    } else {
+        document.documentElement.setAttribute("data-bs-theme", "light");
+        localStorage.setItem("app color-mode", "light");
+        btn_color_mode.className = "btn btn-dark";
+        btn_color_mode.innerText = "Dark Mode";
+    }
+}
+
+SetDefaults();
+
+btn_play.addEventListener("click", function() {
+    
+});
+
+btn_color_mode.addEventListener("click", function() {
+    ChangeColorMode();
+});
